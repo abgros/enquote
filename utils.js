4c6ad7ea0da94c44e2e91e6b52b01e3d3955f5f0
abgros <{ID}+{username}@users.noreply.github.com>
2025-07-28 19:46:57 -0400

    Update to support NYT and Guardian

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-export const browserAPI = typeof browser !== "undefined" ? browserAPI : chrome;
+export const browserAPI = typeof browser !== "undefined" ? browser : chrome;
 
 export async function runInTab(tabId, func, args = []) {
 	const [{result}] = await browserAPI.scripting.executeScript({target: {tabId}, func, args});
