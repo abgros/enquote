3391bdbe017713ccb3d27ec559e61117e07e3c4e
abgros <58993004+abgros@users.noreply.github.com>
2025-07-28 00:00:51 -0400

    tweak permissions, formatting

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 export const browserAPI = typeof browser !== "undefined" ? browserAPI : chrome;
 
 export async function runInTab(tabId, func, args = []) {
-	const [{ result }] = await browserAPI.scripting.executeScript({target: {tabId}, func, args});
+	const [{result}] = await browserAPI.scripting.executeScript({target: {tabId}, func, args});
 	return result;
 }
